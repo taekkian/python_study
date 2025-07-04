3d5db1f97f69d6a7b2dff94e47f3a6b295d7917f
dongs <taekkian@gmail.com>
2025-07-04 09:27:46 +0900

    20250704 modified update

--- Pytion/20250703_test.py
+++ Pytion/20250703_test.py
@@ -1,15 +1,56 @@
-x = int(input("첫 번째? "))
-y = int(input("두 번째? "))
-sum = x + y
-sub = x - y
-mul = x * y
-avg = sum / 2
-num_max = max (x, y)
-num_min = min (x, y)
+# 1
+# x = int(input("첫 번째? "))
+# y = int(input("두 번째? "))
+# sum = x + y
+# sub = x - y
+# mul = x * y
+# avg = sum / 2
+# num_max = max (x, y)
+# num_min = min (x, y)
 
-print("수의 합 %d" %sum)
-print("수의 차 %d" %sub)
-print("수의 곱 %d" %mul)
-print("평균 %d" %avg)
-print("큰 수 %d" %num_max)
-print("작은 수 %d" %num_min)
+# print("수의 합 %d" %sum)
+# print("수의 차 %d" %sub)
+# print("수의 곱 %d" %mul)
+# print("평균 %d" %avg)
+# print("큰 수 %d" %num_max)
+# print("작은 수 %d" %num_min)
+
+# 2
+# chicken_leg = 2
+# pig_leg = 4
+# cow_leg = 4
+# total_leg = chicken_leg*2 + pig_leg*4 + cow_leg*3
+# print("전체 다리는 %d개입니다" %total_leg)
+
+# 3
+# tri_side_a = int(input("삼각형 첫 번째 변? "))
+# tri_side_b = int(input("삼각형 두 번째 변? "))
+# max_side = tri_side_a + tri_side_b -1
+# print("삼각형의 나머지 변의 최대 길이는 %d입니다." %max_side)
+
+# 4
+# hour = int(input("시간을 입력하세요 :"))
+# min  = int(input("분을 입력하시오 : "))
+# trans_sec = hour*3600 + min*60
+# print("%d시 %d분을 초로 변환하면 %d초입니다." %(hour, min, trans_sec))
+
+# 5
+# f_temp = float(input("화씨온도를 입력하세요 :"))
+# c_temp = (f_temp - 32.0)*5/9
+# print("입력된 화씨온도 %.2f를 섭씨온도로 변환하면 %.2f입니다." %(f_temp, c_temp))
+
+# 6
+# cost_of_food = int(input("음식비용을 입력하시오 : "))
+# tips = cost_of_food * 0.1
+# total_cost = cost_of_food + tips
+# print("음식비용과 팁비용을 포함한 전체 비용은 %.1f입니다." %total_cost)
+
+# # 7
+# pos_x1 = float(input("x1 좌표를 입력하시오 : "))
+# pos_y1 = float(input("y1 좌표를 입력하시오 : "))
+# pos_x2 = float(input("x2 좌표를 입력하시오 : "))
+# pos_y2 = float(input("y2 좌표를 입력하시오 : "))
+# distance = ((pos_x1-pos_x2)**2 +(pos_y1-pos_y2)**2)**(1/2)
+# print("두점 (%.1f,%.1f)과 (%.1f,%.1f) 사이의 거리는 %.2f입니다." %(pos_x1, pos_y1, pos_x2, pos_y2, distance))
+
+
